42a61adca664bad9a12296e8196e237147cf69a6
Melisa Morales Gomez <melisamoralesgo@gmail.com>
2021-10-11 09:07:05 -0500

    ContenidoDelSitio

---
+++ Controllers/ConExpresionRegular.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppWeb_Regex.Models;
+using System.IO;
+
+namespace AppWeb_Regex.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ConExpresionRegular : ControllerBase
+    {
+        [HttpGet("ConsultarValidacion/{Asunto}")]
+        public bool Get(string Asunto)
+        {
+            return (new ExpresionRegular().ValidarExpresionRegular(Asunto));
+        }
+
+        //public bool Post(string Asunto)
+        //{
+        //    return (new ExpresionRegular().ValidarExpresionRegular(Asunto));
+        //}
+    }
+}
---
+++ Models/ExpresionRegular.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace AppWeb_Regex.Models
+{
+    public class ExpresionRegular
+    {
+        public ExpresionRegular(){}
+
+        public bool ValidarExpresionRegular(string valor)
+        {
+            Regex reT = new Regex(@"[A-Z]*[:]*[\s]*[0-9]*[-]*[0-9]*[;]\s*([A-Z]{0,}[a-z]{0,}[0-9]{0,}[\s]*[.]*[-]*)*");
+            MatchCollection mat = reT.Matches(valor);
+            foreach (Match match in mat)
+                return true;                
+            return false;
+        }
+
+    }
+}
